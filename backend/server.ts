242476c2886b3bbb3a02cf7b783b5216cdfdd700
raulmaqueda2 <[200~pl2023322@365.stucom.com>
2025-05-12 18:39:10 +0200

    fix: solved error shoket

--- backend/server.ts
+++ backend/server.ts
@@ -8,27 +8,22 @@ import dashboard from './routes/dashboard';
 import messageRoutes from './routes/messages';
 
 const app = express();
-const PORT = 5001;
+const PORT_CHAT = 5001;
+const PORT_EDITOR = 5002;
 
-// Crear el servidor HTTP
-const server = http.createServer(app);
+// Crear el servidor HTTP para el chat
+const serverChat = http.createServer(app);
 
-// Inicializar WebSocket para chat
-initChatWebSocket(server, '/chat');
+// Crear el servidor HTTP para el editor
+const serverEditor = http.createServer(app);
 
-// Inicializar WebSocket para editor
-initEditorWebSocket(server, '/editor');
+// Inicializar WebSocket para chat en el servidorChat
+initChatWebSocket(serverChat, '/chat');
+
+// Inicializar WebSocket para editor en el servidorEditor
+initEditorWebSocket(serverEditor, '/editor');
 
 // Middleware para habilitar CORS
-// IMPORTANT!!!
-// npx ts-node server.ts  --> para ejecutar el servidor
-// npx ts-node-dev server.ts  --> para ejecutar y q se reinicie automáticamente el servidor al guardar cambios
-
-
-// Inicializar el chat
-initChatWebSocket(server, '/chat');
-
-// Middleware para habilitar CORS: permite que el servidor acepte solicitudes de diferentes orígenes (del otro puerto del frontend)
 app.use(cors());
 
 // Middleware para procesar JSON
@@ -39,7 +34,12 @@ app.use('/api', authRoutes);
 app.use('/dashboard', dashboard);
 app.use('/api/messages', messageRoutes);
 
-// Iniciar el servidor
-server.listen(PORT, () => {
-  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+// Iniciar el servidor para el chat
+serverChat.listen(PORT_CHAT, () => {
+  console.log(`Servidor Chat escuchando en http://localhost:${PORT_CHAT}`);
+});
+
+// Iniciar el servidor para el editor
+serverEditor.listen(PORT_EDITOR, () => {
+  console.log(`Servidor Editor escuchando en http://localhost:${PORT_EDITOR}`);
 });
